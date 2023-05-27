4c2ea6c330cb35d03ceecd7f51335e3fa8973dfa
Mannkumar_K._Pandya <75202120+Mannkp@users.noreply.github.com>
2023-05-27 14:03:44 +0530

    updated javascript logic and implemented OOP!
    added Object to reduce the amount of code.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const score = document.querySelector("#score");
-const p1 = document.querySelector("#p1");
-const p2 = document.querySelector("#p2");
 const resetBtn = document.querySelector("#reset");
 const selectWinningScore = document.querySelector("#select");
-const p1Score = document.querySelector("#p1Span");
-const p2Score = document.querySelector("#p2Span");
 
-let player1_score = 0;
-let player2_score = 0;
-let winningScore = 0;
+let winningScore = parseInt(selectWinningScore.value);
 let isGameOver = false;
 let isDisabled = false;
+
+const p1 = {
+    score: 0,
+    button: document.querySelector("#p1"),
+    display: document.querySelector("#p1Span")
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector("#p2"),
+    display: document.querySelector("#p2Span")
+}
 
 selectWinningScore.addEventListener("change", function() {
     winningScore = parseInt(this.value);
@@ -20,71 +25,86 @@ selectWinningScore.addEventListener("change", function() {
 
 resetBtn.addEventListener('click', reset);
 
-function reset(){
-    player1_score = 0;
-    player2_score = 0;
-    isGameOver = false;
-    p1Score.innerText = player1_score;
-    p2Score.innerText = player2_score;
-    p1Score.classList.remove("red" , "green");
-    p2Score.classList.remove("red" , "green");
-    p1.disabled = false;
-    p2.disabled = false;
-    isDisabled = false;
-    p1.classList.remove("grey");
-    p2.classList.remove("grey");
-}
-
-p1.addEventListener('click', function(){
-    if (player1_score !== winningScore){
-        if(!isGameOver){
-            player1_score += 1;
-        }
-        if(player1_score === winningScore){
+function updateScores(player, opponent){
+    if(!isGameOver){
+        player.score += 1;
+        if(player.score === winningScore){
             isGameOver = true;
-            console.log("player 1 wins!");
-            p1Score.classList.add('green');
-            p2Score.classList.add('red');
-            p1.disabled = true;
-            p2.disabled = true;
+            player.display.classList.add('green');
+            opponent.display.classList.add('red');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
             isDisabled = true;
             if(isDisabled){
                 // p1.style.backgroundColor = "grey";
                 // p2.style.backgroundColor = "grey";
-                p1.classList.add("grey");
-                p2.classList.add("grey");
+                player.button.classList.add("grey");
+                opponent.button.classList.add("grey");
             }   
         }
-    p1Score.innerText = player1_score;
-}
-});
+    player.display.innerText = player.score;
 
-
-p2.addEventListener('click', function(){
-    if(player2_score !== winningScore){
-        if(!isGameOver){
-            player2_score += 1;
-        }
-        if(player2_score === winningScore){
-            isGameOver = true;
-            console.log("player 2 wins!");
-            p1Score.classList.add('red');
-            p2Score.classList.add('green');
-            p1.disabled = true;
-            p2.disabled = true;
-            isDisabled = true;
-            if(isDisabled){
-                // p1.style.backgroundColor = "grey";
-                // p2.style.backgroundColor = "grey";
-                p1.classList.add("grey");
-                p2.classList.add("grey");
-            }
-        }
-    p2Score.innerText = player2_score;
     }
+}
+
+function reset(){
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        isGameOver = false;
+        p.display.innerText = p.score;
+        p.display.classList.remove("red" , "green");
+        p.button.disabled = false;
+        isDisabled = false;
+        p.button.classList.remove("grey");
+    }
+}
+
+p1.button.addEventListener('click', function(){
+    updateScores(p1,p2);    
+});
+p2.button.addEventListener('click', function(){
+    updateScores(p2,p1);    
 });
 
-    // if(isGameOver){
-    //     p1.disabled = true;
-    //     p2.disabled = true;
-    // }
+// p1.addEventListener('click', function(){
+//         if(!isGameOver){
+//             player1_score += 1;
+//         }
+//         if(player1_score === winningScore){
+//             isGameOver = true;
+//             console.log("player 1 wins!");
+//             p1Score.classList.add('green');
+//             p2Score.classList.add('red');
+//             p1.disabled = true;
+//             p2.disabled = true;
+//             isDisabled = true;
+//             if(isDisabled){
+//                 // p1.style.backgroundColor = "grey";
+//                 // p2.style.backgroundColor = "grey";
+//                 p1.classList.add("grey");
+//                 p2.classList.add("grey");
+//             }   
+//         }
+//     p1Score.innerText = player1_score;
+// });
+
+
+// p2.addEventListener('click', function(){
+//         if(!isGameOver){
+//             player2_score += 1;
+//         }
+//         if(player2_score === winningScore){
+//             isGameOver = true;
+//             console.log("player 2 wins!");
+//             p1Score.classList.add('red');
+//             p2Score.classList.add('green');
+//             p1.disabled = true;
+//             p2.disabled = true;
+//             isDisabled = true;
+//             if(isDisabled){
+//                 p1.classList.add("grey");
+//                 p2.classList.add("grey");
+//             }
+//         }
+//     p2Score.innerText = player2_score;
+// });
